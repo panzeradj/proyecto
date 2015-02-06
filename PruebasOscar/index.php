d7ae4294666033b3b704277330ae829e17ae6484
Oscar Romero <o0kiii@hotmail.com>
2015-02-06 08:39:09 +0100

    Probando Oscar

---
+++ PruebasOscar/index.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Autobuses AJAX</title>
+	<link rel="stylesheet" href="estilo.css">
+	<link rel="icon" type="image/png" href="" />
+	<script src="control.js" type="text/javascript" charset="utf-8" async defer></script>
+</head>
+<body id="principal">
+	<section id="sesion">
+		<form action="navegacion.html" method="POST" accept-charset="utf-8">
+			<strong>Usuario</strong> <br>
+     		<input type="text" name="usuario" class="campo" required><br>
+    		<strong>Clave</strong> <br>
+      		<input type="password" name="contrasena" class="campo" required><br>
+     		<input type="submit" name="Submit" class="boton" value="Enviar">
+		</form>	
+	</section>
+</body>
+</html>
