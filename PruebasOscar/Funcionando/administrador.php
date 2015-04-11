09fdbe31b814c5587ce920beed3990e28c95687a
Oscar Romero <o0kiii@hotmail.com>
2015-04-11 18:51:07 +0200

    ASD

--- PruebasOscar/Funcionando/administrador.php
+++ PruebasOscar/Funcionando/administrador.php
@@ -3,20 +3,7 @@ include("menu.php");
 ?>
 <!DOCTYPE html>
 <html lang="es">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="AC Wellness">
-    <meta name="author" content="Oscar Romero">
-    <title>AC Wellness</title>
-    <link href="bootstrap/bootstrap.min.css" rel="stylesheet">
-    <link href="bootstrap/logo-nav.css" rel="stylesheet">
-    <link rel="stylesheet" href="estilos/stylesadmin.css">
-</head>
-<body>
 <?php
-echo menu();
+menu();
 ?>
-</body>
 </html>
--- PruebasOscar/MenuFinal/menu.php
+++ PruebasOscar/MenuFinal/menu.php
@@ -2,11 +2,12 @@
 
 
 function menu (){
-	return "<body>
-    <!-- Navigation -->
+	echo "<head>
+
+	</head>
+	<body>
     <nav class='navbar navbar-inverse navbar-fixed-top' role='navigation'>
         <div class='container'>
-            <!-- Brand and toggle get grouped for better mobile display -->
             <div class='navbar-header'>
                 <button type='button' class='navbar-toggle' data-toggle='collapse' data-target='#bs-example-navbar-collapse-1'>
                     <span class='sr-only'>Toggle navigation</span>
@@ -18,7 +19,6 @@ function menu (){
                     <img src='imagenes/e.png' id='logo'>
                 </a>
             </div>
-            <!-- Collect the nav links, forms, and other content for toggling -->
             <div class='collapse navbar-collapse' id='bs-example-navbar-collapse-1'>
                 <ul class='nav navbar-nav'>
                     <li>
@@ -43,12 +43,9 @@ function menu (){
 
                 </ul>
             </div>
-            <!-- /.navbar-collapse -->
         </div>
-        <!-- /.container -->
     </nav>
 
-    <!-- Page Content -->
     <div class='container'>
         <div class='row'>
             <div class='col-lg-12'>
@@ -56,12 +53,9 @@ function menu (){
             </div>
         </div>
     </div>
-    <!-- /.container -->
 
-    <!-- jQuery -->
     <script src='js/jquery.js'></script>
 
-    <!-- Bootstrap Core JavaScript -->
     <script src='js/bootstrap.min.js'></script></body>";
 }
 ?>
