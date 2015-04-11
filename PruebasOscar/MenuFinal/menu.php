5ec790a30547474340b915f164df5fd8f49bbc79
Oscar Romero <o0kiii@hotmail.com>
2015-04-11 18:54:02 +0200

    5645

--- PruebasOscar/MenuFinal/menu.php
+++ PruebasOscar/MenuFinal/menu.php
@@ -3,7 +3,15 @@
 
 function menu (){
 	echo "<head>
-
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <meta name='description' content='AC Wellness'>
+    <meta name='author' content='Oscar Romero'>
+    <title>AC Wellness</title>
+    <link href='bootstrap/bootstrap.min.css' rel='styleheet'>
+    <link href='bootstrap/logo-nav.css' rel='stylesheet'>
+    <link rel='stylesheet' href='estilos/stylesadmin.css'>
 	</head>
 	<body>
     <nav class='navbar navbar-inverse navbar-fixed-top' role='navigation'>
