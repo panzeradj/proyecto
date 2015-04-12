f7748f2e05bfd7e888f10277096ea90217bdc0c5
Oscar Romero <o0kiii@hotmail.com>
2015-04-12 21:09:07 +0200

    Ultimo cambio del funcionamiento de INICIO y MENU
    FINAL

--- PruebasOscar/Funcionando/administrador.php
+++ PruebasOscar/Funcionando/administrador.php
@@ -3,7 +3,65 @@ include("menu.php");
 ?>
 <!DOCTYPE html>
 <html lang="es">
-<?php
-menu();
-?>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible content=IE=edge">
+    <meta name="viewport content=width=device-width, initial-scale=1">
+    <meta name="description" content="AC Wellness">
+    <meta name="author" content="Oscar Romero">
+    <title>AC Wellness</title>
+    <link href="bootstrap/bootstrap.min.css"s rel="stylesheet">
+    <link href="bootstrap/logo-nav.css" rel="stylesheet">
+    <link rel="stylesheet" href="estilos/stylesadmin.css">
+</head>
+<body>
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">
+                    <img src="imagenes/e.png" id="logo">
+                </a>
+            </div>
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li>
+                        <a href="#">Clientes</a>
+                    </li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Reservas <b class="caret"></b></a>
+                        <ul class="dropdown-menu">
+                          <li><a href="#">Individuales</a></li>
+                          <li><a href="#">Multiples</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="#">Calendario</a>
+                    </li>
+                    <li>
+                        <a href="#">Tarifas y bonos</a>
+                    </li>
+                    <li>
+                        <a href="#">Configuracion</a>
+                    </li>
+                </ul>
+            </div>
+
+        </div>
+    </nav>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+            </div>
+        </div>
+    </div>
+    <script src="js/jquery.js"></script>
+    <script src="js/bootstrap.min.js"></script>
 </html>
--- PruebasOscar/Funcionando/index.php
+++ PruebasOscar/Funcionando/index.php
@@ -9,19 +9,19 @@
 
     <link rel="stylesheet" href="bootstrap/bootstrap.min.css">
     <link rel="stylesheet" href="bootstrap/bootstrap-theme.min.css">
-    <link rel="stylesheet" href="estilos/stylesadmin.css">
+    <link rel="stylesheet" href="estilos/styles.css">
 </head>
 
 <body>
     <div class="container">
         <div class="col-md-12">
             <div class="col-md-4"></div>
-            <div class="col-md-4" id="avatar">
-                <form class="form-signin" role="form" action="administrador.php" method="POST" accept-charset="utf-8">
+            <div class="col-md-4" id="login">
+                 <form class="form-signin" role="form" action="administrador.php" method="POST" accept-charset="utf-8">
                     <div class="text-center">
-                        <img id="avatar" src="imagenes/logos.png" alt="avatar">
+                        <img id="avatar" src="imagenes/logos.png" >
                     </div>
-                    <input id="txtEmail" type="user" class="form-control" placeholder="Usuario">
+                    <input id="txtEmail" class="form-control" placeholder="Usuario">
                     <input type="password" class="form-control" placeholder="Contraseña">
                     <button class="btn btn-lg btn-primary btn-block" type="submit">Iniciar sesión</button>
                 </form>
@@ -32,7 +32,7 @@
     <script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
     <script src="jquery/jquery.md5.min.js"></script>
     <script src="bootstrap/bootstrap.min.js"></script>
-    <script src="script/script.js"></script>
+    <script src="scripts/script.js"></script>
 </body>
 
 </html>
