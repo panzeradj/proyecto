5d02519c74523be2a07a7fb81e7358570d0ef869
elisatg <elisataranconguedan@gmail.com>
2015-04-27 17:52:13 +0200

    Revert "Cálculo de facturas afinado"
    This reverts commit 8bfc2c87142122d17897053613e03368a0d3644f.

--- PruebasElisa/php/edicion.php
+++ PruebasElisa/php/edicion.php
@@ -15,8 +15,15 @@ if (isset($_POST["accion"])){
 			$factura=$_POST["factura"];
 			$valor=$_POST["valor"];
 			if ($_POST["valor"]==0){
-				$valorfactura=preciofactura($factura);
-				ordensqlupdate("UPDATE facturas set valor=".$valorfactura." where id_factura=".$factura.";");
+				$listaprecio=ordensql("SELECT valor_sin_iva*count(*)
+							from precios_tarifas p, contratos c, tarifas t, facturas f, lineas_factura lf 
+							where c.tarifa=p.tarifa and p.tarifa=id_tarifa and f.cliente=c.cliente and f.id_factura=lf.factura	
+							and fecha_inicial=(select fecha_inicial
+							                    from precios_tarifas p,contratos c
+							                    where c.tarifa=p.tarifa and factura=".$factura."
+							                    order by fecha_inicial desc limit 1);");
+				$resultadoprecio=$listaprecio->fetch_array();
+				ordensqlupdate("UPDATE facturas set valor=".$resultadoprecio[0]." where id_factura=".$factura.";");
 			}elseif ($valor>0){
 				ordensqlupdate("UPDATE facturas set valor=".$valor." where id_factura=".$factura.";");
 			}
