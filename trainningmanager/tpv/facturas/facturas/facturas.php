ace43a338ef65de6e7eb06a5a639b204d6c8231a
Oscar Romero <o0kiii@hotmail.com>
2015-05-25 21:15:20 +0200

    cosas extrañas que nose porque salen :toilet:

--- trainningmanager/tpv/facturas/facturas/facturas.php
+++ trainningmanager/tpv/facturas/facturas/facturas.php
@@ -1,7 +1,10 @@
 <?php
 include ("../../funciones/funciones.php");
+include('./fpdf.php');
 $conexion=abre();
+echo "<script>alert(".$_POST["generar_factura"].")</script>";
 if ($_POST["generar_factura"] == "true")
+
 {
 
 //Recibir detalles de factura
@@ -15,7 +18,7 @@ $fecha_factura = $registro[0];
 
 
 //Recibir los datos de la empresa
-$nombre_tienda = "AC wellness S.L";
+$nombre_tienda = "Training Manager S.L";
 $direccion_tienda = "C/ Calle San José Obrero";
 $poblacion_tienda = "Soria";
 $provincia_tienda = "Soria";
@@ -35,7 +38,8 @@ $apellidos_cliente =$registro[1];
 $archivo="factura-$nfactura.pdf";
 
 //Llamada al script fpdf
-require('fpdf.php');
+
+echo $apellidos_cliente;
 
 
 $archivo_de_salida=$archivo;
@@ -91,6 +95,7 @@ false);
 
 //Salto de línea
 $pdf->Ln(2);
+echo "llega";
 
 
 
@@ -98,6 +103,7 @@ $pdf->Ln(2);
 $e_productos = explode(",", $productos);
 $e_unidades = explode(",", $unidades);
 $e_precio_unidad = explode(",", $precio_unidad);
+
 
 //Creación de la tabla de los detalles de los productos productos
 $top_productos = 110;
@@ -112,7 +118,7 @@ $top_productos = 110;
     $pdf->SetXY(145, $top_productos);
     $pdf->Cell(40, 5, 'PRECIO TOTAL', 0, 1, 'C');   
  
-$subtotal= 0; // variable para almacenar el subtotal
+$precio_subtotal = 0; // variable para almacenar el subtotal
 $y = 115; // variable para la posición top desde la cual se empezarán a agregar los datos
 $x=0;
 
@@ -132,26 +138,24 @@ $pdf->SetFont('Arial','',8);
     $pdf->Cell(40, 5, $registro[1]." ", 0, 1, 'C');
     $pdf->SetXY(145, $y);
     $pdf->Cell(40, 5, $registro[1]*$registro[2]." ", 0, 1, 'C');
-
+echo $registro[3];
 //Cálculo del subtotal 	
 $subtotal=$registro[1]*$registro[2]+$subtotal;
 
 // aumento del top 5 cm
 $y = $y + 5;
 }
-$clases=0;
 $orden="SELECT producto from lineas_factura where factura=".$nfactura." and producto<200000";
-if ($chorizo = $conexion->query($orden)){
+if ($reserva = $conexion->query($orden)){
             
             
         // Recorremos el resultset fila a fila
-         while ($registro = $chorizo->fetch_array()) {
+         while ($registro = $reserva->fetch_array()) {
                     $cod_clase=$registro[0];
-                    $orden="SELECT anyo, mes, dia, cliente, cancelada from reservas where id_reserva=".$registro[0].";";
-                    if ($reserva = $conexion->query($orden)){
-                        $fecha= $reserva->fetch_array();
+                    $orden="SELECT anyo, mes, dia, cliente from reservas where id_reserva=".$registro[0].";";
+                    if ($chorizo = $conexion->query($orden)){
+                        $fecha= $chorizo->fetch_array();
                         //echo $fecha[0].$fecha[1].$fecha[2].$fecha[3];
-                        $estadoclase=$fecha[4];
                         if ($fecha[1]<10){
                             if ($fecha[2]<10) {
                                 $dia=$fecha[0]."-0".$fecha[1]."-0".$fecha[2];
@@ -165,30 +169,23 @@ if ($chorizo = $conexion->query($orden)){
                                 $dia=$fecha[0]."-".$fecha[1]."-".$fecha[2];
                             }
                         }
-                        $orden="SELECT valor_sin_iva,p.tarifa from precios_tarifas p,contratos c
+                        $orden="SELECT valor_sin_iva from precios_tarifas p,contratos c
                                     where c.tarifa=p.tarifa and cliente=".$fecha[3]."
                                     and fecha_inicial<'".$dia."'
                                     order by fecha_inicial desc limit 1;";
                                     //echo $orden;
+
+
+                        
                         $listavalor= $conexion->query($orden);
                         $resultadoprecio=$listavalor->fetch_array();
                         $precio=$resultadoprecio[0];
-                        $tarifa=$resultadoprecio[1];
-                        $orden="SELECT nombre FROM tarifas p where id_tarifa=".$tarifa.";";
-                        $des= $conexion->query($orden);
-                        $nombre=$des->fetch_array();
-                        $nombretarifa="Clase ".$nombre[0]." (".$dia.")";
-
-                        if($estadoclase==2){
-                            $nombretarifa="Clase ".$nombre[0]." (".$dia.") (Con recargo)";
-                        }
-
                         $pdf->SetFont('Arial','',8);
 
                         $pdf->SetXY(15, $y);
                         $pdf->Cell(40, 5, $cod_clase, 0, 1, 'C');   
                         $pdf->SetXY(50, $y);
-                        $pdf->Cell(40, 5,$nombretarifa, 0, 1, 'C');
+                        $pdf->Cell(40, 5, "Sesión de Entrenamiento", 0, 1, 'C');
                         $pdf->SetXY(85, $y);
                         $pdf->Cell(40, 5, 1, 0, 1, 'C');
                         $pdf->SetXY(115, $y);
@@ -196,15 +193,15 @@ if ($chorizo = $conexion->query($orden)){
                         $pdf->SetXY(145, $y);
                         $pdf->Cell(40, 5, $precio." ", 0, 1, 'C');
 
-                    //Cálculo del subtotal
-                        $clases=$precio+$clases;
-                         $y = $y + 5; 
+                    //Cálculo del subtotal  
+                        $subtotal=$precio+$subtotal;
+                        $y = $y + 5;
 
                     }
                     
          }
 }
-  $subtotal=$subtotal+$clases;
+
 $orden="SELECT p.descripcion, p.valor,p.id_producto FROM productos_libres p,lineas_factura l
 where p.id_producto=l.producto and l.factura=".$nfactura.";";
 $chorizo = $conexion->query($orden);
@@ -229,81 +226,13 @@ $subtotal=$registro[1]+$subtotal;
 // aumento del top 5 cm
 $y = $y + 5;
 }
-$orden="SELECT f.valor,f.descuento, (f.valor/100)*f.descuento, i.iva, f.estado FROM facturas f, iva i where f.id_factura=".$nfactura.";";
+$orden="SELECT f.valor,f.descuento, (f.valor/100)*f.descuento as descuento_aplicado,i.iva FROM facturas f, iva i where f.id_factura=".$nfactura.";";
                
         if ($chorizo = $conexion->query($orden)){
-             $registro = $chorizo->fetch_array();
-            if($registro[4]==0){
-                $editado=$clases-$registro[0];
-
-                if($editado!=0){
-                    if($editado>0){
-                        $pdf->SetFont('Arial','',8);
-                        $pdf->SetXY(15, $y);
-                        $pdf->Cell(40, 5, 0, 0, 1, 'C');   
-                        $pdf->SetXY(50, $y);
-                        $pdf->Cell(40, 5, "Calse(s) adelantadas", 0, 1, 'C');
-                        $pdf->SetXY(85, $y);
-                        $pdf->Cell(40, 5, 1, 0, 1, 'C');
-                        $pdf->SetXY(115, $y);
-                        $pdf->Cell(40, 5, $editado." ", 0, 1, 'C');
-                        $pdf->SetXY(145, $y);
-                        $pdf->Cell(40, 5, $editado." ", 0, 1, 'C');
-
-                        $y = $y + 5;
-                    }else{
-                        $pdf->SetFont('Arial','',8);
-                        $pdf->SetXY(15, $y);
-                        $pdf->Cell(40, 5, 0, 0, 1, 'C');   
-                        $pdf->SetXY(50, $y);
-                        $pdf->Cell(40, 5, "Calse(s) descontadas", 0, 1, 'C');
-                        $pdf->SetXY(85, $y);
-                        $pdf->Cell(40, 5, 1, 0, 1, 'C');
-                        $pdf->SetXY(115, $y);
-                        $pdf->Cell(40, 5, $editado." ", 0, 1, 'C');
-                        $pdf->SetXY(145, $y);
-                        $pdf->Cell(40, 5, $editado." ", 0, 1, 'C');
-
-                        $y = $y + 5;
-                    }
-                }
-            }else{
-                $editado=$subtotal-$registro[0];
-
-                if($editado!=0){
-                    if($editado>0){
-                        $pdf->SetFont('Arial','',8);
-                        $pdf->SetXY(15, $y);
-                        $pdf->Cell(40, 5, 0, 0, 1, 'C');   
-                        $pdf->SetXY(50, $y);
-                        $pdf->Cell(40, 5, "Calse(s) adelantadas", 0, 1, 'C');
-                        $pdf->SetXY(85, $y);
-                        $pdf->Cell(40, 5, 1, 0, 1, 'C');
-                        $pdf->SetXY(115, $y);
-                        $pdf->Cell(40, 5, $editado." ", 0, 1, 'C');
-                        $pdf->SetXY(145, $y);
-                        $pdf->Cell(40, 5, $editado." ", 0, 1, 'C');
-
-                        $y = $y + 5;
-                    }else{
-                        $pdf->SetFont('Arial','',8);
-                        $pdf->SetXY(15, $y);
-                        $pdf->Cell(40, 5, 0, 0, 1, 'C');   
-                        $pdf->SetXY(50, $y);
-                        $pdf->Cell(40, 5, "Calse(s) descontadas", 0, 1, 'C');
-                        $pdf->SetXY(85, $y);
-                        $pdf->Cell(40, 5, 1, 0, 1, 'C');
-                        $pdf->SetXY(115, $y);
-                        $pdf->Cell(40, 5, $editado." ", 0, 1, 'C');
-                        $pdf->SetXY(145, $y);
-                        $pdf->Cell(40, 5, $editado." ", 0, 1, 'C');
-
-                        $y = $y + 5;
-                    }
-                }
-
-            }
-               
+            
+        // Recorremos el resultset fila a fila
+                $registro = $chorizo->fetch_array();
+                //echo $orden;
                 $suma=$subtotal;
                 $descuento=($suma/100)*$registro[1];
                 $des=$registro[1];
@@ -315,10 +244,7 @@ $orden="SELECT f.valor,f.descuento, (f.valor/100)*f.descuento, i.iva, f.estado F
 
 //Cálculo del Impuesto
 //$add_iva = $precio_subtotal * $iva / 100;
-$descuento=round($descuento, 2);
-$ivacal=round($ivacal, 2);
-$subtotal=round($subtotal, 2);
-$total=round($total, 2);
+
 //Cálculo del precio total
 //$total_mas_iva = round($precio_subtotal + $add_iva + $gastos_de_envio, 2);
 
@@ -343,5 +269,5 @@ fpassthru($fp);
 fclose($fp);
 
 //Eliminación del archivo en el servidor
-unlink($archivo);
+unlink($archivo);*/
 }
