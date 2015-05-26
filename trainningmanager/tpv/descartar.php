4f350b452cb76a2530f304be24111b62706d0b6c
FinubarDK <juanrodriguezaguilera@hotmail.com>
2015-05-26 16:23:13 +0200

    Corregir
    También subo la parte de Rober.

--- trainningmanager/tpv/descartar.php
+++ trainningmanager/tpv/descartar.php
@@ -1,30 +1,28 @@
 <?php
 	include ("./funciones/funciones.php");
 	$nfactura=$_POST['nfactura'];
+	echo($nfactura);
 	if($nfactura!=null){
 		$conexion=abre();
 		$orden="select cliente from facturas WHERE id_factura=".$nfactura.";";
 		$conexion->query($orden);
 		$chorizo = $conexion->query($orden);
 		$registro = $chorizo->fetch_array();
+		echo($nfactura);
 		
-
 		if($registro[0]==1){
-			$orden="SELECT f.borrable FROM facturas f where id_factura=".$nfactura.";";
-			$chorizo=$conexion->query($orden);
-			$registro = $chorizo->fetch_array();
-
-			if($registro[0]==0){
-				$orden="delete FROM lineas_factura  where producto>199999 and factura=$nfactura;";
+			$orden="delete FROM lineas_factura  where producto>199999 and factura=$nfactura;";
 				$conexion->query($orden);
-				fijartotal($conexion,$nfactura);
+				$orden="delete FROM facturas  where id_factura=$nfactura;";
+				$conexion->query($orden);
+	
+				echo("aqui");
 
 
 			}else{
 				$orden="delete FROM lineas_factura  where producto>199999 and factura=$nfactura;";
 				$conexion->query($orden);
-				$orden="delete FROM facturas  where id_factura=$nfactura;";
-				$conexion->query($orden);
+				fijartotal($conexion,$nfactura);
 				
 			}
 			$mensaje="eliminados";
@@ -32,9 +30,7 @@
 			$mensaje="errcre";
 		}
 	cierra($conexion);
-}else{
-	$mensaje="errnocre";
-}	
+
 
 	header ("Location: tpv.php?mensaje=$mensaje");
 ?>
